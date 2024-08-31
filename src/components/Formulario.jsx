9f856afe73337e972d8e0267ce68ff0f827290f0
Catalina <catalinacardenas459@gmail.com>
2024-08-31 14:22:14 -0500

    Se termino la estructura, faltan estilos

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,7 +1,8 @@
 import { useForm } from 'react-hook-form';
+import { edadValidator } from './Validators';
 
 const Formulario = () =>{
-    const {register, handleSubmit} = useForm ();
+    const {register, formState: {errors},  handleSubmit} = useForm ();
 
     const onSubmit = (date) =>{
         console.log(date);
@@ -14,17 +15,38 @@ const Formulario = () =>{
         <form onSubmit={handleSubmit(onSubmit)}>
             <div>
                 <label>Nombre:</label>
-                <input type='text' name=''/>
+                <input type='text' {...register('nombre', {
+                    required: true,
+                    maxLength: 50
+                } )}/>
+                {errors.nombre?.type === 'required' && <p>Este campo es requerido</p>}
+                {errors.nombre?.type === 'maxLength' && <p>El campo nombre debe tener menos de 50 caracteres</p>}
             </div>
 
             <div>
                 <label>Direccion:</label>
-                <input type='text' name=''/>
+                <input type='text' {...register('direccion' , {
+                    required: true,
+                    maxLength: 50
+                })}/>
+                {errors.direccion?.type === 'required' && <p>Este campo es requerido</p>}
+                {errors.direccion?.type === 'maxLength' && <p>El campo direccion debe tener menos de 50 caracteres</p>}
+            </div>
+
+            <div>
+                <label>Email:</label>
+                <input type='text' {...register('email' , {
+                    pattern: /^[\s@]+@[^\s@]+\.[^\s@]+$/i
+                })}/>
+                {errors.email?.type === 'pattern' && <p>El formato de email es incorrecto</p>}
             </div>
 
             <div>
                 <label>Edad:</label>
-                <input type='text' name=''/>
+                <input type='text' {...register('edad', {
+                    validate: edadValidator
+                })}/>
+                {errors.edad && <p>La edad debe estar entre 1 y 100 años</p>}
             </div>
 
             <div>
